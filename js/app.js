7af538916d40d2ae70a1399cb3c754868e7e8216
vindu07 <andrea.vinduska@gmail.com>
2025-09-18 15:31:19 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.toggle("light-mode");
   });
 });
+
+
+// registra il service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js")
+    .then(() => console.log("Service Worker registrato"))
+    .catch(err => console.error("Errore Service Worker:", err));
+}
