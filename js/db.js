0749951bcd3df02561eeffc1ae60ae0afcba215d
vindu07 <andrea.vinduska@gmail.com>
2025-09-23 20:59:21 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -27,6 +27,8 @@ async function createTask(task) {
   console.log("Task salvato con ID:", docRef.id);
   return docRef.id;
 }
+async function editTask(ID, task){}
+async function deleteTask(ID){}
 
 /*LEGGE I RECORD E LI MANDA AL RENDERER*/
 async function loadTasks() {
@@ -49,7 +51,7 @@ async function loadTasks() {
 
 
 
-const TASKS_KEY = "tasks";
+
 
 
 
@@ -61,27 +63,3 @@ function sortTaskBySubject(subject){}
 
 function saveTasks(){}
 function getTasks(){}
-
-
-
-function getTasks(){
-    try{ return JSON.parse(localStorage.getItem(TASKS_KEY)||"[]"); }
-    catch(e){ console.warn("Errore parsing tasks", e); return []; }
-}
-
-function saveTasks(tasks){
-    try{ localStorage.setItem(TASKS_KEY, JSON.stringify(tasks)); }
-    catch(e){ console.warn("Errore salvataggio tasks", e); }
-}
-
-function addOrUpdateTask(task){
-    let tasks = getTasks();
-    if(!task.id) task.id = Date.now().toString();
-    const idx = tasks.findIndex(t=>t.id===task.id);
-    if(idx>=0) tasks[idx]=task; else tasks.push(task);
-    saveTasks(tasks);
-}
-
-function deleteTask(id){
-    saveTasks(getTasks().filter(t=>t.id!==id));
-}
