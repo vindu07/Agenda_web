ba5bf36f0f2b20498d6cd5eb741e3244fbcbcdb3
vindu07 <andrea.vinduska@gmail.com>
2025-09-23 19:25:43 +0200

    Update diary.js

--- js/diary.js
+++ js/diary.js
@@ -2,6 +2,7 @@ const weekdays = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "
 const months = ["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"];
 
 let currentDate = new Date();
+let pagDiario; //data della pagina corrente
 
 const weekdayEl = document.getElementById("weekday-name");
 const dayNumberEl = document.getElementById("day-number");
@@ -13,6 +14,9 @@ function updateDiary() {
   weekdayEl.textContent = weekdays[currentDate.getDay()];
   dayNumberEl.textContent = currentDate.getDate();
   monthYearEl.textContent = `${months[currentDate.getMonth()]} ${currentDate.getFullYear()}`;
+  //aggiorna la variabile con la data della pagina corrente
+  pagDiario = currentDate.toISOString().slice(0,10);
+
 }
 
 // Eventi per cambiare giorno
