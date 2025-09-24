8a9250b7124a3a60deeef489630d920d227dd792
vindu07 <andrea.vinduska@gmail.com>
2025-09-24 17:21:48 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,31 +1,50 @@
 /*IMPORTA FUNZIONI DI FIRESTORE FIREBASE*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyBrc58aNKEHfGueKron2D87g3tlAWWqBlU",
+    authDomain: "agenda-pwa-b5bb0.firebaseapp.com",
+    projectId: "agenda-pwa-b5bb0",
+    storageBucket: "agenda-pwa-b5bb0.firebasestorage.app",
+    messagingSenderId: "211012466659",
+    appId: "1:211012466659:web:feaf2be83c99a55ae5d2d6",
+    measurementId: "G-5V4TVEFWEC"
+  };
+const app = initializeApp(firebaseConfig);
+
 import { 
   getFirestore, collection, addDoc, getDocs, 
   doc, getDoc, updateDoc, deleteDoc, 
   query, where, orderBy 
 } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
-const db = getFirestore(); // già inizializzato col tuo app
+const db = getFirestore(app); // già inizializzato col tuo app
 const tasksRef = collection(db, "tasks");
+
+
+import * as Tasks from "./tasks.js";
+import { pagDiario } from "./diary.js";
 
 
 /*CREA TASK*/
 async function createTask(task) {
   // Trasforma l'array in oggetto
   const task_obj = {
-    ID: task[0],
-    scadenza: task[1],
-    materia: task[2],
-    isTest: task[3],
-    priority: task[4],
-    desc: task[5],
-    isCompleted: task[6]
+    
+    scadenza: task[0],
+    materia: task[1],
+    isTest: task[2],
+    priority: task[3],
+    desc: task[4],
+    isCompleted: task[5]
   };
 
   // Salva in Firestore
   const docRef = await addDoc(tasksRef, task_obj);
   console.log("Task salvato con ID:", docRef.id);
+  alert("Compito salvato correttamente!");
   return docRef.id;
+    //render
+    loadTasks();
 }
 async function editTask(ID, task){}
 async function deleteTask(ID){}
@@ -42,12 +61,13 @@ async function loadTasks() {
     console.log("Tasks caricati:", tasks);
 
     // Passa l'array a taskRender
-    renderTasks(tasks);
+    Tasks.renderTasks(tasks);
 
   } catch (err) {
     console.error("Errore nel caricamento dei task:", err);
   }
 }
+
 
 
 
