f3507ee15059a8ad9590ab111d6abd6b5d051ee7
vindu07 <andrea.vinduska@gmail.com>
2025-09-24 20:50:29 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -113,7 +113,7 @@ export async function sortTasks(options = {}) {
     if (conditions.length > 0) {
       q = query(q, ...conditions);
     }
-    console.log("Condizioni filtro date:", tsInizio, tsFine);
+    console.log("Condizioni filtro date:", startTS, endTS);
 
     const querySnapshot = await getDocs(q);
 
