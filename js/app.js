0db7cd9f4d338faa344c259e4004de0f3281cb7c
vindu07 <andrea.vinduska@gmail.com>
2025-09-24 17:12:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -121,7 +121,7 @@ if ("serviceWorker" in navigator) {
 document.getElementById("save-task").addEventListener("click", () => {
   
   // raccogli valori dall'HUD
-  const scadenza = 0;
+  const scadenza = pagDiario;
   const isCompleted = false;
   const desc = document.getElementById("task-desc").value.trim();
   const materia = document.getElementById("task-subject").value;
