0c9ca52ceb21075ae8f3ec10286d871a0a08f0c4
vindu07 <andrea.vinduska@gmail.com>
2025-09-24 21:34:53 +0200

    Update diary.js

--- js/diary.js
+++ js/diary.js
@@ -29,14 +29,15 @@ function updateDiary() {
 
   /*chiama sortTasks*/
   
-  const ts = Timestamp.fromDate(currentDate);
-  const ts1 = ts.toDate();
+  const ts = new Date(pagDiario);//Timestamp.fromDate(currentDate);
+  //const ts1 = ts.toDate();
 
 // 2️⃣ aggiungo 1 giorno (24 ore)
-ts1.setDate(ts1.getDate() + 1);
+//ts1.setDate(ts1.getDate() + 1);
 
 // 3️⃣ converto di nuovo in Timestamp
-const TS = Timestamp.fromDate(ts1)
+const TS = ts;//Timestamp.fromDate(ts1)
+TS.setDate(TS.getDate() + 1);
 
   db.sortTasks({ dataInizio: ts,
   dataFine: TS /*,materia: "Italiano"*/ })
