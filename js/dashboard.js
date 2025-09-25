68dd4aad9c1e9dba499d589484790bdfb45a9cba
vindu07 <andrea.vinduska@gmail.com>
2025-09-25 17:18:39 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,12 @@
 import * as db from "./db.js";
 import * as Tasks from "./tasks.js";
 import * as settings from "./settings.js";
+
+const numGiorni = 4; //quanti giorni visualizza
+
+const oggi = new Date();
+const termine = oggi; termine.setDate(oggi.getdate() + 1 + numGiorni);
+
+const parametri = { isCompleted: false, startDate: oggi, endDate: termine }
+
+Tasks.renderTasks(db.sortTasks(parametri)); //seleziona e visualizza i task
