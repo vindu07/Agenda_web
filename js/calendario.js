e7bbde9a9d8dfe889e0b6ce567cef13694c81e81
vindu07 <andrea.vinduska@gmail.com>
2025-09-25 21:13:01 +0200

    Update and rename calendar.js to calendario.js

---
+++ js/calendario.js
@@ -0,0 +1,15 @@
+const days = document.querySelectorAll("#calendar .day-number");
+
+// metto i numeri dei giorni
+days.forEach((el, i) => {
+  el.textContent = i + 1; // esempio: 1..35
+});
+
+// aggiungo 3 pallini al giorno 10
+const dotsDiv = document.querySelectorAll("#calendar .tasks")[9];
+["red", "green", "blue"].forEach(c => {
+  const d = document.createElement("div");
+  d.className = `dot ${c}`;
+  dotsDiv.appendChild(d);
+});
+
