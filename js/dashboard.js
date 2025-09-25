df3933661b10e3a751b397d2137fecb3689d2d73
vindu07 <andrea.vinduska@gmail.com>
2025-09-25 17:52:52 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,13 @@ const numGiorni = 4; //quanti giorni visualizza
 const oggi = Timestamp.fromDate(new Date());
 const termine = new Date(); termine.setDate(oggi.getdate() + 1 + numGiorni); termine = Timestamp.fromDate(termine);
 
-const parametri = { isCompleted: false/*, dataInizio: oggi, dataFine: termine */}
 
 Tasks.renderTasks(db.sortTasks(parametri)); //seleziona e visualizza i task
+db.sortTasks(parametri)
+  .then(sortedTasks => {
+    // qui sortedTasks è l'array ordinato
+    Tasks.renderTasks(sortedTasks);
+  })
+  .catch(err => {
+    console.error("Errore nel caricamento/ordinamento dei task:", err);
+  });
